853ce9dc1b7c779d57b5eed0bb0df793f3b51282
dailyBotG3 <99742215+dailyBotG3@users.noreply.github.com>
2022-02-16 16:18:34 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ const DailyBotG3 = require('node-telegram-bot-api');
 const TOKEN = '5265779985:AAFaFCMeQsAo4kfeUfVqon9dOa8atYn0dfY';
 
 const bot = new DailyBotG3(TOKEN, {polling:true});
-let q = new Date();
-let m = q.getMonth();
-let d = q.getDay();
-let y = q.getFullYear();
 
-let date = new Date(y,m,d);
+let today = new Date().toLocaleDateString()
 
-
-bot.on('message', (message) => {
-    console.log(message);
- 
-});
+bot.onText(/Daily/, function (msg) {
+    console.log(msg);
+    let chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Préparez vous pour le Daily du '+today);
+  })
 
 setInterval(() => {
     
-bot.sendMessage(-623609888, 'Daily du '+date);
+bot.sendMessage(-623609888, 'Préparez vous pour le Daily du '+today);
+bot.sendMessage( 5160627421, 'Préparez vous pour le Daily du '+today);
 
  }, 86400000);
+
+
+ 
